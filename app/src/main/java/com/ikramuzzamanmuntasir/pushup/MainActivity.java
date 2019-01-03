265e9cc627afdb420e8b2c78d078151e2eb636b2
muntasir14 <ikramuzzaman.muntasir@gmail.com>
2019-01-04 00:21:47 +0600

    Initial commit

---
+++ app/src/main/java/com/ikramuzzamanmuntasir/pushup/MainActivity.java
@@ -0,0 +1,18 @@
+package com.ikramuzzamanmuntasir.pushup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void testUpload() {
+
+        int i = 1;
+    }
+}
